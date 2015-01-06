db763d4e3adba97767f576d98992d006e9f0ea80
wilbown <wilbown@gmail.com>
2015-01-05 21:52:16 -0700

    - added stream redirect to status text box - created tabs, layout for scaling

--- src/main/java/com/bconomy/autobit/FXMLController.java
+++ src/main/java/com/bconomy/autobit/FXMLController.java
@@ -1,7 +1,11 @@
 package com.bconomy.autobit;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -10,24 +14,55 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 
 public class FXMLController implements Initializable {
-    
-    @FXML
-    private TextArea statusBox;
+
+	@FXML
+	private TextArea statusBox;
 	private Button btnStart;
 	private Button btnStop;
-    
-    @FXML
-    private void onBtnStart(ActionEvent event) {
-        System.out.println("You clicked Start!");
-    }
+
+	@FXML
+	private void onBtnStart(ActionEvent event) {
+		System.out.println("You clicked Start!");
+	}
+
+	@FXML
+	private void onBtnStop(ActionEvent event) {
+		System.out.println("You clicked Stop!");
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		redirectSystemStreams(statusBox);
+	}
 	
-    @FXML
-    private void onBtnStop(ActionEvent event) {
-        System.out.println("You clicked Stop!");
-    }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+	
+	//redirect System.out to a textbox
+	private static void updateTextArea(final TextArea ta, final String text) {
+		Platform.runLater(new Runnable() {
+			@Override
+			public void run() {
+				int len = ta.getLength();
+				if (len > 30000) ta.setText(ta.getText(len - 20000, len));
+				ta.appendText(text);
+			}
+		});
+	}
+	public static void redirectSystemStreams(final TextArea ta) {
+		OutputStream out = new OutputStream() {
+			@Override
+			public void write(int b) throws IOException {
+				updateTextArea(ta, String.valueOf((char) b));
+			}
+			@Override
+			public void write(byte[] b, int off, int len) throws IOException {
+				updateTextArea(ta, new String(b, off, len));
+			}
+			@Override
+			public void write(byte[] b) throws IOException {
+				write(b, 0, b.length);
+			}
+		};
+		System.setOut(new PrintStream(out, true));
+		System.setErr(new PrintStream(out, true));
+	}
 }
