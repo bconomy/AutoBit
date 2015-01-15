9e515eaaaa60c23e2fea5918f63b3489764b0822
wilbown <wilbown@gmail.com>
2015-01-15 16:44:48 -0700

    - remove encrypted preferences stuff, going to put in encrypted prefs in an xml file

--- src/main/java/com/bconomy/autobit/FXMLController.java
+++ src/main/java/com/bconomy/autobit/FXMLController.java
@@ -15,32 +15,44 @@
  */
 package com.bconomy.autobit;
 
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ResourceBundle;
-import java.util.prefs.BackingStoreException;
-import java.util.prefs.Preferences;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TextArea;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.DESKeySpec;
+import javafx.scene.control.TextField;
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonReader;
+import javax.json.JsonWriter;
 
 public class FXMLController implements Initializable {
 	public static FXMLController instance;
-	private static final DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS ");
+	
+	//TODO create timestamped logging
+	//private static final DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS ");
 	//df.format(new Date())
+	//String ln = String.format("%1$tF %1$tT %2$-20s %3$s\r\n", new java.util.Date(), loc, status);
 
 	public RunThread runThread;
 	public class RunThread extends Thread {
@@ -63,24 +75,58 @@ public class FXMLController implements Initializable {
 	
 	//*******************GUI
 	@FXML
-	private TextArea statusBox;
-	private Button btnStart;
-	private Button btnStop;
+	private TextArea fxStatusBox;
+	@FXML
+	private TextField fxTextFieldCBAPIKey;
+	@FXML
+	private PasswordField fxPasswordFieldCBAPISecret;
+	@FXML
+	private TextField fxTextFieldBCWallet;
+	
+	
+	@FXML
+	private void onTabSettingsSelected(Event event) {
+		if (!((Tab)event.getSource()).isSelected()) {
+			saveSettings();
+			System.out.println("Settings saved.");
+		}
+	}
 
 	@FXML
-	private void onBtnStart(ActionEvent event) {
+	private void onButtonStart(ActionEvent event) {
 		System.out.println("******** Start");
 		
 		runThread = new RunThread();
 		runThread.start();
 	}
 	@FXML
-	private void onBtnStop(ActionEvent event) {
+	private void onButtonStop(ActionEvent event) {
 		System.out.println("******** Stop");
 		
 		if (runThread != null) {
 			runThread.stop = true;
 		}
+	}
+	@FXML
+	private void onHyperlinkCBGetAPIKey(ActionEvent event) {
+		System.out.println("******** onHyperlinkCBGetAPIKey");
+		
+		if (Desktop.isDesktopSupported()) {
+			try {
+				Desktop.getDesktop().browse(new URI("https://coinbase.com/settings/api"));
+			} catch (IOException | URISyntaxException ex) {
+				ex.printStackTrace();
+			}
+		}
+		//javafx.application.HostServices test = new HostServices(this);
+//		HostServicesDelegate hostServices = HostServicesFactory.getInstance(this);
+//		hostServices.showDocument("http://www.yahoo.com");
+		//this.getHostServices().showDocument("http://www.yahoo.com");
+//		try {
+//			new ProcessBuilder("x-www-browser", "\"https://coinbase.com/settings/api\"").start();
+//		} catch (IOException ex) {
+//			ex.printStackTrace();
+//		}
 	}
 
 	
@@ -88,20 +134,63 @@ public class FXMLController implements Initializable {
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		instance = this;
-		redirectSystemStreams(statusBox);
-			
+		redirectSystemStreams(fxStatusBox);
 		
+		
+		//settings
+//		Preferences root = Preferences.userRoot();
+//		//Preferences root = Preferences.userRoot().node("/com/coremake/advertising");
+//		//Preferences root = Preferences.userNodeForPackage(this.getClass());
+//		System.out.println(root.absolutePath());
+		
+//		Encryption.key = root.getByteArray("item", null);
+//		if (Encryption.key == null) {
+//			Encryption.makeKey();
+//			root.putByteArray("item", Encryption.key);
+//		}
+//		try {
+//			root.flush();
+//		} catch (BackingStoreException ex) {
+//			ex.printStackTrace();
+//		}
+		
+//		if (!settingsFile.exists()) settingsFile.getParentFile().mkdirs();
+//		else loadSettings();
 	}
     public void close() {
 		if (runThread != null) {
 			runThread.stop = true;
 			runThread.interrupt();
 		}
+		saveSettings();
 	}
 	
 	
 	//*******************Util
-	//redirect System.out to a textbox
+	private final static File settingsFile = new File(System.getProperty("user.home"), "AutoBitSettings.json");
+    private void saveSettings() {
+		try (FileOutputStream fs = new FileOutputStream(settingsFile); JsonWriter json = Json.createWriter(fs);) {
+			JsonObjectBuilder builder = Json.createObjectBuilder()
+				.add("BCWallet", fxTextFieldBCWallet.getText())
+				.add("CBAPISecret", Encryption.encrypt(fxPasswordFieldCBAPISecret.getText()))
+			;
+			json.writeObject(builder.build());
+		} catch (IOException | NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException ex) {
+			ex.printStackTrace();
+		}
+    }
+    private void loadSettings() {
+		if (!settingsFile.exists()) return;
+		try (FileInputStream fs = new FileInputStream(settingsFile); JsonReader json = Json.createReader(fs);) {
+			JsonObject model = json.readObject();
+			if (model.containsKey("BCWallet")) fxTextFieldBCWallet.setText(model.getString("BCWallet", ""));
+			if (model.containsKey("CBAPISecret")) fxPasswordFieldCBAPISecret.setText(Encryption.decrypt(model.getString("CBAPISecret", "")));
+		} catch (IOException | NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	//redirect System.out to a TextArea
 	private static void updateTextArea(final TextArea ta, final String text) {
 		Platform.runLater(new Runnable() {
 			@Override
